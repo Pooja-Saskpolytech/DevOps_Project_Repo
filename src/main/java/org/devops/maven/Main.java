a3e62ae751d2c729de1e8a3850ead3aea65bb2f3
Pooja Vijayan Nair <123132457+Pooja-Saskpolytech@users.noreply.github.com>
2023-04-16 14:58:08 -0600

    updated target folder

--- src/main/java/org/devops/maven/Main.java
+++ src/main/java/org/devops/maven/Main.java
@@ -1,7 +1,13 @@
 package org.devops.maven;
+import java.awt.GraphicsEnvironment;
+
 public class Main {
     public static void main(String[] args) {
+        if (!GraphicsEnvironment.isHeadless()) {
+            AppFrame frame = new AppFrame();
+            frame.setVisible(true);
+        }
+        System.out.println("Continuing execution...");
 
-        AppFrame frame = new AppFrame();
     }
 }
